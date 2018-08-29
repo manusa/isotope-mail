ee62ca0a1006cdcae7e1995cfa0908666169a00f
Marc Nuri <marc@marcnuri.com>
2018-08-29 07:21:22 +0200

    #41: Back Button

--- client/src/components/message-viewer/message-viewer.jsx
+++ client/src/components/message-viewer/message-viewer.jsx
@@ -60,6 +60,21 @@ class MessageViewer extends Component {
       </div>
     );
   }
+
+  componentDidMount() {
+    DOMPurify.addHook('afterSanitizeAttributes', node => {
+      // set all elements owning target to target=_blank
+      if ('target' in node) {
+        node.setAttribute('target','_blank');
+      }
+      // set non-HTML/MathML links to xlink:show=new
+      if (!node.hasAttribute('target')
+        && (node.hasAttribute('xlink:href')
+          || node.hasAttribute('href'))) {
+        node.setAttribute('xlink:show', 'new');
+      }
+    });
+  }
 }
 
 MessageViewer.propTypes = {
