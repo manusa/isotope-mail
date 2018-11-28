2e3e23235333ff442091cdf4931af9fcf6554add
Marc Nuri <marc@marcnuri.com>
2018-11-28 06:53:24 +0100

    #134: FE service completely covered

--- client/src/services/__tests__/message.test.js
+++ client/src/services/__tests__/message.test.js
@@ -1,19 +1,26 @@
 import * as messageService from '../message';
 import * as messageActions from '../../actions/messages';
 import * as fetch from '../fetch';
+import {ActionTypes} from '../../actions/action-types';
 
 describe('Message service test suite', () => {
   describe('deleteMessages', () => {
-    test('deleteMessages with valid message array, should fetch and dispatch results', () => {
+    test('deleteMessages with valid message array, should fetch and dispatch results', done => {
       // Given
       global.fetch = jest.fn((url, options) => {
         expect(url.toLocaleString()).toMatch('http://test.url/?id=1&id=1337');
-        return Promise.resolve({ok: true, url, options, json: Promise.resolve({})});
+        return Promise.resolve({ok: true, url, options, json: () => Promise.resolve({fromServer: true})});
       });
       fetch.abortFetch = jest.fn();
       messageActions.deleteFromCache = jest.fn();
       messageActions.setSelected = jest.fn();
-      const dispatch = jest.fn();
+      const dispatch = jest.fn()
+        .mockImplementation(action => {
+          if (action && action.type === ActionTypes.FOLDERS_UPDATE) {
+            expect(action.payload.fromServer).toEqual(true);
+            done();
+          }
+        });
       const credentials = {};
       const folder = {_links: {messages: {href: 'http://test.url'}}};
       const messages = [{uid: 1}, {uid: 1337}];
@@ -25,6 +32,30 @@ describe('Message service test suite', () => {
       expect(fetch.abortFetch).toHaveBeenCalledTimes(1);
       expect(messageActions.deleteFromCache).toHaveBeenCalledTimes(1);
       expect(messageActions.setSelected).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+    });
+    test('deleteMessages with valid message array, fetch has error, should dispatch previous folder/messages', done => {
+      // Given
+      global.fetch = jest.fn(url => {
+        expect(url.toLocaleString()).toMatch('http://test.url.witherror/?id=1&id=1337');
+        return Promise.resolve({ok: true});
+      });
+      fetch.abortFetch = jest.fn();
+      const dispatch = jest.fn()
+        .mockImplementation(action => {
+          if (action && action.type === ActionTypes.FOLDERS_UPDATE) {
+            expect(action.payload.originalFolder).toEqual(true);
+            done();
+          }
+        });
+      const credentials = {};
+      const folder = {_links: {messages: {href: 'http://test.url.witherror'}}, originalFolder: true};
+      const messages = [{uid: 1}, {uid: 1337}];
+
+      // When
+      messageService.deleteMessages(dispatch, credentials, folder, messages);
+
+      // Then
       expect(global.fetch).toHaveBeenCalledTimes(1);
     });
     test('deleteMessages with empty message array, should do nothing', () => {
