4305be0abdecce5592113bd8ececf610bc90cff5
Marc Nuri <marc@marcnuri.com>
2019-01-11 07:32:09 +0100

    #166 #114: Unit test to validate Redux store reducer

--- client/src/reducers/__tests__/folders.test.js
+++ client/src/reducers/__tests__/folders.test.js
@@ -13,5 +13,72 @@ describe('Folders reducer test suite', () => {
     const updatedState = folders(INITIAL_STATE.folders, {type: ActionTypes.FOLDERS_BE_REQUEST});
     expect(updatedState.activeRequests).toBe(1);
   });
+  describe('APPLICATION_FOLDER_RENAME_OK', () => {
+    test('Folder renamed no longer exists in current state, should return unchanged state', () => {
+      // Given
+      const stateFolders = {...INITIAL_STATE.folders};
+
+      // When
+      const updatedState = folders(stateFolders,
+        {type: ActionTypes.APPLICATION_FOLDER_RENAME_OK, payload: {oldFolderId: 'notInList'}});
+
+      // Then
+      expect(updatedState).toStrictEqual(INITIAL_STATE.folders);
+    });
+    test('Folder renamed with children in root,' +
+      'should remove folder from previous tree position and children from exploded tree',
+    () => {
+      // Given
+      const stateFolders = {...INITIAL_STATE.folders};
+      stateFolders.explodedItems.parent = {folderId: 'parent',
+        children: [{folderId: 'child1'}, {folderId: 'child2'}]};
+      stateFolders.explodedItems.child1 = stateFolders.explodedItems.parent.children[0];
+      stateFolders.explodedItems.child2 = stateFolders.explodedItems.parent.children[1];
+      stateFolders.explodedItems.parentToRemain = {folderId: 'parentToRemain',
+        children: [{folderId: 'parentToRemainChild'}]};
+      stateFolders.explodedItems.parentToRemainChild = stateFolders.explodedItems.parentToRemain.children[0];
+      stateFolders.items = [stateFolders.explodedItems.parent, stateFolders.explodedItems.parentToRemain];
+
+      // When
+      const updatedState = folders(stateFolders,
+        {type: ActionTypes.APPLICATION_FOLDER_RENAME_OK, payload: {oldFolderId: 'parent'}});
+
+      // Then
+      expect(updatedState.items.length).toEqual(1);
+      expect(Object.keys(updatedState.explodedItems).length).toEqual(2);
+      expect(updatedState.explodedItems).toMatchObject({
+        parentToRemain: expect.anything(),
+        parentToRemainChild: expect.anything()
+      });
+      expect(updatedState.items[0]).toMatchObject(stateFolders.explodedItems.parentToRemain);
+    });
+    test('Folder renamed with children in folder,' +
+      'should remove folder from previous tree position and children from exploded tree',
+    () => {
+      // Given
+      const stateFolders = {...INITIAL_STATE.folders};
+      stateFolders.explodedItems.parent = {folderId: 'parent',
+        children: [{folderId: 'child1'}, {folderId: 'child2'}]};
+      stateFolders.explodedItems.child1 = stateFolders.explodedItems.parent.children[0];
+      stateFolders.explodedItems.child2 = stateFolders.explodedItems.parent.children[1];
+      stateFolders.explodedItems.parentToRemain = {folderId: 'parentToRemain',
+        children: [parent, {folderId: 'parentToRemainChild'}]};
+      stateFolders.explodedItems.parentToRemainChild = stateFolders.explodedItems.parentToRemain.children[0];
+      stateFolders.items = [stateFolders.explodedItems.parentToRemain];
+
+      // When
+      const updatedState = folders(stateFolders,
+        {type: ActionTypes.APPLICATION_FOLDER_RENAME_OK, payload: {oldFolderId: 'parent'}});
+
+      // Then
+      expect(updatedState.items.length).toEqual(1);
+      expect(Object.keys(updatedState.explodedItems).length).toEqual(2);
+      expect(updatedState.explodedItems).toMatchObject({
+        parentToRemain: expect.anything(),
+        parentToRemainChild: expect.anything()
+      });
+      expect(updatedState.items[0]).toMatchObject(stateFolders.explodedItems.parentToRemain);
+    });
+  });
 });
 
--- client/src/reducers/folders.js
+++ client/src/reducers/folders.js
@@ -24,8 +24,10 @@ function _updateFolder(folderToReplace, folders) {
 
 const _deleteChildFolder = toDeleteFolderId => {
   const deleteChildFolderFunction = folder => {
-    folder.children = folder.children.filter(child => child.folderId !== toDeleteFolderId);
-    folder.children.forEach(deleteChildFolderFunction);
+    if (Array.isArray(folder.children)) {
+      folder.children = folder.children.filter(child => child.folderId !== toDeleteFolderId);
+      folder.children.forEach(deleteChildFolderFunction);
+    }
   };
   return deleteChildFolderFunction;
 };
@@ -49,13 +51,29 @@ const folders = (state = INITIAL_STATE.folders, action = {}) => {
       newUpdateState.explodedItems = explodeFolders(newUpdateState.items);
       return newUpdateState;
     }
+    /**
+     * Once a folder is renamed, previous references to that folder should be removed from the tree and exploded
+     * tree state.
+     * Other actions will have already taken care of adding the renamed folder to the new position, this action
+     * is only responsible for the removal process.
+     * This action should be triggered whenever a folder is renamed or moved within a mailbox.
+     */
     case ActionTypes.APPLICATION_FOLDER_RENAME_OK: {
       const newState = {...state};
       // Remove previous folder from Tree
       const {oldFolderId} = action.payload;
       if (Object.keys(newState.explodedItems).includes(oldFolderId)) {
-        // Delete from Exploded list
-        delete newState.explodedItems[oldFolderId];
+        // Delete from Exploded list (Parent + Children)
+        const explodedFoldersToDeleteIds = [];
+        const gatherDeletedFolderAndChildrenIds = folder => {
+          explodedFoldersToDeleteIds.push(folder.folderId);
+          if (Array.isArray(folder.children)) {
+            folder.children.forEach(gatherDeletedFolderAndChildrenIds);
+          }
+        };
+        gatherDeletedFolderAndChildrenIds(newState.explodedItems[oldFolderId]);
+        explodedFoldersToDeleteIds.forEach(folderToDeleteId => delete newState.explodedItems[folderToDeleteId]);
+
         // Delete from tree (root folders)
         newState.items = newState.items.filter(item => item.folderId !== oldFolderId);
         // Delete from tree (child folders)
