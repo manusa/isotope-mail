d9298f6e937745d37976bdcb624deaf86e50e94e
Marc Nuri <marc@marcnuri.com>
2019-03-23 09:42:34 +0100

    #211: Removed code smell

--- client/src/reducers/login.js
+++ client/src/reducers/login.js
@@ -3,15 +3,12 @@ import {ActionTypes} from '../actions/action-types';
 
 
 const login = (state = INITIAL_STATE.login, action = {}) => {
-  switch (action.type) {
-    case ActionTypes.LOGIN_FORM_VALUES_SET: {
-      const newState = {...state, formValues: {...action.payload}};
-      delete newState.formValues.password;
-      return newState;
-    }
-    default:
-      return state;
+  if (action.type === ActionTypes.LOGIN_FORM_VALUES_SET) {
+    const newState = {...state, formValues: {...action.payload}};
+    delete newState.formValues.password;
+    return newState;
   }
+  return state;
 };
 
 export default login;
