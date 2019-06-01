fdf459d2596c3bc34ddba873fb14ce362d6a85a4
Marc Nuri <marc@marcnuri.com>
2019-06-01 18:09:34 +0200

    #309: SideBar is collapsed when application not in desktop

--- client/src/services/__tests__/configuration.test.js
+++ client/src/services/__tests__/configuration.test.js
@@ -1,4 +1,4 @@
-import {fetchConfiguration} from '../configuration';
+import {fetchConfiguration, isDesktop} from '../configuration';
 
 describe('Configuration service test suite', () => {
   describe('fetchConfiguration', () => {
@@ -20,6 +20,38 @@ describe('Configuration service test suite', () => {
       // Then
       expect(json).toHaveBeenCalledTimes(1);
       expect(result).toEqual(configuration);
+    });
+  });
+  describe('isDesktop', () => {
+    beforeAll(() => {
+      // document....clientWidth as property
+      Object.defineProperty(document.documentElement, 'clientWidth', {value: 0, writable: true});
+    });
+    test('Desktop window viewport, should return true', () => {
+      // Given
+      window.innerWidth = 601;
+      // When
+      const result = isDesktop();
+      // Then
+      expect(result).toBe(true);
+    });
+    test('Desktop document viewport, should return true', () => {
+      // Given
+      window.innerWidth = 500;
+      document.documentElement.clientWidth = 601;
+      // When
+      const result = isDesktop();
+      // Then
+      expect(result).toBe(true);
+    });
+    test('Mobile viewport, should return true', () => {
+      // Given
+      window.innerWidth = 599;
+      document.documentElement.clientWidth = 594;
+      // When
+      const result = isDesktop();
+      // Then
+      expect(result).toBe(false);
     });
   });
 });
